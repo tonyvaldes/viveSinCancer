5bb7bcb4732c273779c971caa8fefb8cbaa81a4e
tonyvaldes <tonyvaldes.231@gmail.com>
2018-01-17 08:44:38 -0700

    tienda en linea y su css

--- js/hideDiv.js
+++ js/hideDiv.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
     $("#whatToDo2").hide();
     $("#whatToDo3").hide();
     $("#whatToDo4").hide();
+    $("#availableProducts").show();
+    $("#availableEvents").hide();
+    $("#availableDonations").hide();
+    $("#shoppingCart").hide();
+    $("#checkout").hide();
+
+
+
 
     $("#whatToDoAction1").click(function () {
         $('#whatToDo1').fadeToggle('slow');
@@ -38,5 +46,45 @@ $(document).ready(function(){
         $("#whatToDo3").hide();
         $("#whatToDoImageCaption").hide();
     });
+    $("#showProducts").click(function () {
+        $('#availableProducts').fadeToggle('slow');
+        $("#availableEvents").hide();
+        $("#availableDonations").hide();
+        $("#shoppingCart").hide();
+        $("#checkout").hide();
+    });
+    $("#showEvents").click(function () {
+        $('#availableProducts').hide();
+        $("#availableEvents").fadeToggle('slow');
+        $("#availableDonations").hide();
+        $("#shoppingCart").hide();
+        $("#checkout").hide();
+    });
+    $("#showDonations").click(function () {
+        $('#availableProducts').hide();
+        $("#availableEvents").hide();
+        $("#availableDonations").fadeToggle('slow');
+        $("#shoppingCart").hide();
+        $("#checkout").hide();
+    });
+
+    $("#showShoppingCart").click(function () {
+        $('#availableProducts').hide();
+        $("#availableEvents").hide();
+        $("#availableDonations").hide();
+        $("#shoppingCart").fadeToggle('slow');
+        $("#checkout").hide();
+    });
+
+    $("#showCheckout").click(function () {
+        $('#availableProducts').hide();
+        $("#availableEvents").hide();
+        $("#availableDonations").hide();
+        $("#shoppingCart").hide();
+        $("#checkout").fadeToggle('slow');
+    });
+
+
+
 
 });
